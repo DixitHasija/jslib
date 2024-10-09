ab1eaa5f87ce2cb592dff32ec1708a97f4cd023d
Dixit Kumar Hasija <124657123+DixitHasija@users.noreply.github.com>
2024-10-09 17:45:54 +0530

    Update shopify-app.js

--- shopify-app.js
+++ shopify-app.js
@@ -15,12 +15,12 @@ var sellerPreviewRequest = document.currentScript.src
     (obj, pair) => ((obj[pair.split("=")[0]] = pair.split("=")[1]), obj),
     {}
   )["preview"];
-const promiseLinkElement = document.createElement('link');
+ promiseLinkElement = document.createElement('link');
 promiseLinkElement.rel = 'stylesheet';
 promiseLinkElement.href = 'https://jslib-dixithasijas-projects.vercel.app/instant-widget.css';
 
 // Create script element
-const promiseScriptElement = document.createElement('script');
+ promiseScriptElement = document.createElement('script');
 promiseScriptElement.src = `https://jslib-dixithasijas-projects.vercel.app/app-instant-widget.js?uuid=${sellerUuid}&preview=${sellerPreviewRequest}`
 promiseScriptElement.defer = true; // Add the defer attribute
 
