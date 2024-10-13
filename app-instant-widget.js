dbd76569d69dcc5be780e117e72dc8c59585b39d
Dixit Kumar Hasija <124657123+DixitHasija@users.noreply.github.com>
2024-10-13 22:18:54 +0530

    Update app-instant-widget.js

--- app-instant-widget.js
+++ app-instant-widget.js
@@ -1462,8 +1462,8 @@ async function checkPromiseActiveStatus(token){
     // Overide if Shopify && Shopify?.shop === "zop-by-shiprocket.myshopify.com"
     if (Shopify && Shopify?.shop === "zop-by-shiprocket.myshopify.com") {
       // Commenting for the timing 
-      // promiseABVersion = "D";
-      // localStorage.setItem("_p_version_theme__", promiseABVersion);
+      promiseABVersion = "D";
+      localStorage.setItem("_p_version_theme__", promiseABVersion);
     }
     localStorage.setItem("_p_active_status_", JSON.stringify(promiseData));
     return showInPreview == true
