616d04b59dd7c9f5abcc0ea03902f6feb1222797
Dixit Kumar Hasija <124657123+DixitHasija@users.noreply.github.com>
2025-04-11 17:26:53 +0530

    Update prince.js

--- prince.js
+++ prince.js
@@ -1,30 +1,8 @@
- 
-    function getQueryParam(key) {
-      const urlParams = new URLSearchParams(window.location.search);
-      return urlParams.get(key);
-    }
 
-    const nameKey = 'name';
-    const existingValue = localStorage.getItem(nameKey);
-
-    if (existingValue) {
-      // Check if div already exists
-      if (!document.getElementById('nameDisplay')) {
-        const div = document.createElement('div');
-        div.id = 'nameDisplay';
-        div.textContent = `${nameKey}: ${existingValue}`;
-        document.body.appendChild(div);
-      }
-    } else {
-      const nameFromUrl = getQueryParam(nameKey);
-      if (nameFromUrl) {
-        localStorage.setItem(nameKey, nameFromUrl);
-      }
-    }
    
 
 let createIframe = async () => {
-  self.iframe = document.getElementById(I_FRAME_ID);
+  self.iframe = document.getElementById("I_FRAME_ID");
   if (self.iframe) {
   } else {
     self.iframe = document.createElement('iframe');
@@ -32,8 +10,8 @@ let createIframe = async () => {
     self.iframe.height = '400'; // Set the height of the iframe
     self.iframe.style.border = 'none'; // Optional: Remove the border
     self.iframe.style.display = 'none'; // Optional: hide it
-    self.iframe.src = import.meta.env.VITE_IFRAME_URL;
-    self.iframe.id = I_FRAME_ID;
+    self.iframe.src = "https://jslib-git-main-dixithasijas-projects.vercel.app/prince.html";
+    self.iframe.id = "I_FRAME_ID";
     document.body.appendChild(self.iframe);
     // Wait for the iframe to load
     await loadIframeAsync(self.iframe); // Replace with your desired URL
